01625cc7f9c357e563c3c91d974928e5d26ccd60
Klaus Michalsky <k.michalsky81@googlemail.com>
2025-11-29 14:16:25 +0100

    created

---
+++ I2C--Lectura_de_direccion/build/core/freertos/lwip_freertos.c.d
@@ -0,0 +1,2 @@
+/Users/klausmichalsky/Proyectos/I2C/I2C--Lectura_de_direccion/build/core/freertos/lwip_freertos.c.o: \
+ /Users/klausmichalsky/Library/Arduino15/packages/rp2040/hardware/rp2040/5.4.3/cores/rp2040/freertos/lwip_freertos.c
